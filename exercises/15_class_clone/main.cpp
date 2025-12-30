7cfa6ed947ffc8d4f0d8995f12bb731766e3f550
JasonLiThirty <jasonlithirty@163.com>
2025-12-30 09:22:50 +0800

    14-18 Done

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -22,7 +22,9 @@ public:
     // TODO: 实现析构器，释放缓存空间
     ~DynFibonacci()
     {
-        delete[] cache;
+        if (cache != nullptr) {
+             delete[] cache;
+        }
     }
 
     // TODO: 实现正确的缓存优化斐波那契计算
